2d1ae8ba892c59b6ce4583a8d9804bc1fa2ae80d
Baisayan <bbbbaisayan@gmail.com>
2026-03-31 12:23:07 +0000

    made 2 const for header n block marker

--- memalloc.c
+++ memalloc.c
@@ -1,5 +1,21 @@
-#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello");
-}
+typedef struct free_area {
+    uint8_t marker;
+    struct free_area *prev;
+    bool in_use;
+    uint32_t length;
+    struct free_area *next;
+} area;
+  
+typedef struct stats {
+    int magical_bytes;
+    bool my_simple_lock;
+    uint32_t amount_of_blocks;
+    uint16_t amount_of_pages;
+} my_stats;
+
+const int MAGICAL_BYTES = 0x55; // header marker 
+const int BLOCK_MARKER = 0xDD;  // block marker
